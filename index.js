620a154ab00b196bfd415bc562dc07d0066bb097
Kayro Henrique <kayrohenriquepereira@gmail.com>
2024-05-07 16:42:47 -0300

    DesadioDIo

---
+++ index.js
@@ -0,0 +1,41 @@
+//desafio heroi
+let nomeHeroi = " Player: Progamador"
+let xpHeroi = " Experiência: 200_XP"
+let vidaHeroi = 100
+const mensagem = "Possui 200xp para subir de nivel?"
+console.log(nomeHeroi + " " + xpHeroi )
+console.log(mensagem)
+vidaHeroi = "Verificando . . ."
+console.log(vidaHeroi)
+xpHeroi = "Para béns Progamador você subiu de nivel após ganhar a quantidade de xp exigida!😎"
+console.log(xpHeroi)
+var inTensjornada = ["Escudo", "Capacete", "Poção de Xp", "Vida Extra"]
+console.log(escolha)
+console.log(inTensjornada)
+console.log(inTensjornada[3]
+)
+let escalandoMasmorras = "Classificações"
+let escalandoMasmorras = "Classificação de patentes🛡"
+let lutaMonstro = "Monstro a frente! dropa 2.000_xp"
+let enfretarMonstro = "Desejar enfrentar?"
+var nn = ["Sim", "Não?"]
+let vida = "Inten extra usado! ➡ 💗VIDA_EXTRA"
+let monstro = " Monstro abatido☠"
+console.log(escalandoMasmorras)
+console.log(lutaMonstro)
+console.log(enfretarMonstro)
+console.log(nn)
+console.log(nn[0])
+console.log(vida)
+
+const parabens = "Parabéns você ganhou 2.000_xp"
+console.log(monstro)
+console.log(parabens)
+let mex = "Você escalou para a patente Bronze🥉"
+let gg = "2.000 points!"
+let gamerLuersaida = "Parabéns pelo esforço _Progamador_!"
+console.log(mex)
+console.log(gg)
+console.log(gamerLuersaida)
+console.log("Continue jogando para obter mais recompensas!")
+
